95781124c30614e1d168f7708ce79a4e1839165e
mahdisedighi <mahdiiiseddighi@gmail.com>
2024-07-13 00:04:51 +0330

    update

--- main.py
+++ main.py
@@ -16,11 +16,13 @@ django.setup()
 
 # Import your models for use in your script
 from db.models import *
-
+from utils.api import Masterkala
 ############################################################################
 ## START OF APPLICATION
 ############################################################################
 """ Replace the code below with your own """
 
 if __name__ == '__main__':
-    pass
+    ma = Masterkala()
+    p = ma.get_product(985)
+    print(p)
--- utils/convert.py
+++ utils/convert.py
@@ -4,23 +4,22 @@ import math
 
 
 def masterkala_to_biid(masterkala_product , before_price=None , main_category=2):
-    discount = 15
 
+    discount = 15
+    pricewithdiscount = int(float(masterkala_product["pricewithdiscount"]))
     if before_price is not None:
         if masterkala_product["super_special"]=="1":
-            if int(float(masterkala_product["pricewithdiscount"])) >= before_price:
-                masterkala_price = int(float(masterkala_product["pricewithdiscount"])) + 100_000
+            if pricewithdiscount >= before_price:
+                masterkala_price = pricewithdiscount + 100_000
             else:
-                
                 masterkala_price = before_price
         else:
-            
-            masterkala_price = int(float(masterkala_product["pricewithdiscount"]))
+            masterkala_price = pricewithdiscount
     else:
         if masterkala_product["super_special"]=="1":
-            masterkala_price = int(float(masterkala_product["pricewithdiscount"])) + 100_000
+            masterkala_price = pricewithdiscount + 100_000
         else:
-            masterkala_price = int(float(masterkala_product["pricewithdiscount"]))
+            masterkala_price = pricewithdiscount
 
 
 
@@ -28,7 +27,10 @@ def masterkala_to_biid(masterkala_product , before_price=None , main_category=2)
         biid_price = masterkala_price + 70_000
     else:
         biid_price = masterkala_price
-    
+
+
+
+
     biid_product = {
         "name": masterkala_product["name"],
         "main_category": main_category,
@@ -36,9 +38,12 @@ def masterkala_to_biid(masterkala_product , before_price=None , main_category=2)
         "compare_at_price": math.ceil(biid_price * 100 / (100 - discount) / 10000) * 10000,
         "product_identifier": masterkala_product["model"],
         "stock": int(masterkala_product["quantity"]),
-        "stock_type": "unlimited" if int(masterkala_product["quantity"]) else "out_of_stock",
+        "stock_type": "unlimited" if int(masterkala_product["quantity"]) or (pricewithdiscount > 1_930_000) else "out_of_stock",
         "barcode": masterkala_product["model"]
     }
+
+
+
     colors = []
     for option in masterkala_product["optionlist"]:
         if "رنگ" in option["name"]:
@@ -47,7 +52,7 @@ def masterkala_to_biid(masterkala_product , before_price=None , main_category=2)
         elif "گارانتی" in option["name"]:
             biid_product["guarantee"] = option["product_option_value"][0]["name"]
 
-    return biid_product, colors
+    return biid_product, colors , pricewithdiscount
 
 
 def hash_product(dictionary):
